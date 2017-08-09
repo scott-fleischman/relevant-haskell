38e86fa9a26d528873b5db6fb428c1403b34d6ef
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-08-09 12:16:08 -0700

    Bulk index movies.

--- src/Main.hs
+++ src/Main.hs
@@ -6,33 +6,44 @@ module Main where
 import qualified Control.Monad.IO.Class as Monad.IO
 import qualified Data.Aeson             as Aeson
 import qualified Data.ByteString.Lazy   as ByteString.Lazy
-import qualified Data.HashMap.Strict    as HashMap.Strict
+import qualified Data.HashMap.Lazy      as HashMap.Lazy
 import qualified Data.Text              as Text
+import qualified Data.Vector            as Vector
 import qualified Database.V5.Bloodhound as Bloodhound
 import qualified Network.HTTP.Client    as HTTP.Client
 
-tmdbPath :: FilePath
-tmdbPath = "relevant-search-book/ipython/tmdb.json"
-
 main :: IO ()
-main = do
+main = reindex
+
+reindex :: IO ()
+reindex = do
   putStrLn $ "Loading " ++ tmdbPath
   bytes <- ByteString.Lazy.readFile tmdbPath
-  tmdb :: HashMap.Strict.HashMap Text.Text Aeson.Value <-
+  tmdb :: HashMap.Lazy.HashMap Text.Text Aeson.Value <-
     case Aeson.eitherDecode bytes of
       Left err -> fail err
       Right x  -> return x
-  putStrLn $ (show . HashMap.Strict.size) tmdb ++ " movies loaded"
+  putStrLn $ (show . HashMap.Lazy.size) tmdb ++ " movies loaded"
 
   let
     server = Bloodhound.Server "http://localhost:9200"
     tmdbIndex = Bloodhound.IndexName "tmdb"
   Bloodhound.withBH HTTP.Client.defaultManagerSettings server $ do
-    Monad.IO.liftIO $ putStrLn "Deleting index"
+    Monad.IO.liftIO $ putStrLn "Deleting index..."
     _ <- Bloodhound.deleteIndex tmdbIndex
 
-    Monad.IO.liftIO $ putStrLn "Creating index"
+    Monad.IO.liftIO $ putStrLn "Creating index..."
     let indexSettings = Bloodhound.IndexSettings (Bloodhound.ShardCount 1) (Bloodhound.ReplicaCount 0)
     _ <- Bloodhound.createIndex indexSettings tmdbIndex
 
+    Monad.IO.liftIO $ putStrLn "Bulk indexing documents..."
+    let
+      movieMapping = Bloodhound.MappingName "movie"
+      makeOperation (movieId, movie) = Bloodhound.BulkIndex tmdbIndex movieMapping (Bloodhound.DocId movieId) movie
+      operations = Vector.fromList . fmap makeOperation . HashMap.Lazy.toList $ tmdb
+    _ <- Bloodhound.bulk operations
+
     return ()
+
+tmdbPath :: FilePath
+tmdbPath = "relevant-search-book/ipython/tmdb.json"
