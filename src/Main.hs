be63d70a0a215a92173eb7d14f0f5c1e7a22c188
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-08-08 17:57:20 -0700

    Add delete/create index.

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,38 @@
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import qualified Data.Aeson           as Aeson
-import qualified Data.ByteString.Lazy as ByteString.Lazy
-import qualified Data.HashMap.Strict  as HashMap.Strict
-import qualified Data.Text            as Text
+import qualified Control.Monad.IO.Class as IO.Class
+import qualified Data.Aeson             as Aeson
+import qualified Data.ByteString.Lazy   as ByteString.Lazy
+import qualified Data.HashMap.Strict    as HashMap.Strict
+import qualified Data.Text              as Text
+import qualified Database.V5.Bloodhound as Bloodhound
+import qualified Network.HTTP.Client    as HTTP.Client
 
 tmdbPath :: FilePath
 tmdbPath = "relevant-search-book/ipython/tmdb.json"
 
 main :: IO ()
 main = do
+  putStrLn $ "Loading " ++ tmdbPath
   bytes <- ByteString.Lazy.readFile tmdbPath
   tmdb :: HashMap.Strict.HashMap Text.Text Aeson.Value <-
     case Aeson.eitherDecode bytes of
       Left err -> fail err
       Right x  -> return x
-  print $ HashMap.Strict.size tmdb
+  putStrLn $ (show . HashMap.Strict.size) tmdb ++ " movies loaded"
+
+  let
+    server = Bloodhound.Server "http://localhost:9200"
+    tmdbIndex = Bloodhound.IndexName "tmdb"
+  Bloodhound.withBH HTTP.Client.defaultManagerSettings server $ do
+    IO.Class.liftIO $ putStrLn "Deleting index"
+    _ <- Bloodhound.deleteIndex tmdbIndex
+
+    IO.Class.liftIO $ putStrLn "Creating index"
+    let indexSettings = Bloodhound.IndexSettings (Bloodhound.ShardCount 1) (Bloodhound.ReplicaCount 0)
+    _ <- Bloodhound.createIndex indexSettings tmdbIndex
+
+    return ()
