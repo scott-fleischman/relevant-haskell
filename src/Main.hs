c3221a52f5e18d852f87366fa08bc1ed9d98d189
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-08-08 17:43:55 -0700

    Load tmdb.json.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
+
+import qualified Data.Aeson           as Aeson
+import qualified Data.ByteString.Lazy as ByteString.Lazy
+import qualified Data.HashMap.Strict  as HashMap.Strict
+import qualified Data.Text            as Text
+
+tmdbPath :: FilePath
+tmdbPath = "relevant-search-book/ipython/tmdb.json"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  bytes <- ByteString.Lazy.readFile tmdbPath
+  tmdb :: HashMap.Strict.HashMap Text.Text Aeson.Value <-
+    case Aeson.eitherDecode bytes of
+      Left err -> fail err
+      Right x  -> return x
+  print $ HashMap.Strict.size tmdb
