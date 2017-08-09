bd4363fb5d8e4e65430b63ca6bf7c3eee77b8c72
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-08-09 13:17:59 -0700

    Add search function.

--- src/Main.hs
+++ src/Main.hs
@@ -27,26 +27,65 @@ extract = do
   return tmdb
 
 reindex :: Aeson.Object -> IO ()
-reindex tmdb = do
+reindex tmdb = Bloodhound.withBH HTTP.Client.defaultManagerSettings server $ do
+  Monad.IO.liftIO $ putStrLn "Deleting index..."
+  _ <- Bloodhound.deleteIndex tmdbIndex
+
+  Monad.IO.liftIO $ putStrLn "Creating index..."
+  let indexSettings = Bloodhound.IndexSettings (Bloodhound.ShardCount 1) (Bloodhound.ReplicaCount 0)
+  _ <- Bloodhound.createIndex indexSettings tmdbIndex
+
+  Monad.IO.liftIO $ putStrLn "Bulk indexing documents..."
   let
-    server = Bloodhound.Server "http://localhost:9200"
-    tmdbIndex = Bloodhound.IndexName "tmdb"
-  Bloodhound.withBH HTTP.Client.defaultManagerSettings server $ do
-    Monad.IO.liftIO $ putStrLn "Deleting index..."
-    _ <- Bloodhound.deleteIndex tmdbIndex
+    makeOperation (movieId, movie) = Bloodhound.BulkIndex tmdbIndex movieMapping (Bloodhound.DocId movieId) movie
+    operations = Vector.fromList . fmap makeOperation . HashMap.Lazy.toList $ tmdb
+  _ <- Bloodhound.bulk operations
 
-    Monad.IO.liftIO $ putStrLn "Creating index..."
-    let indexSettings = Bloodhound.IndexSettings (Bloodhound.ShardCount 1) (Bloodhound.ReplicaCount 0)
-    _ <- Bloodhound.createIndex indexSettings tmdbIndex
+  return ()
 
-    Monad.IO.liftIO $ putStrLn "Bulk indexing documents..."
-    let
-      movieMapping = Bloodhound.MappingName "movie"
-      makeOperation (movieId, movie) = Bloodhound.BulkIndex tmdbIndex movieMapping (Bloodhound.DocId movieId) movie
-      operations = Vector.fromList . fmap makeOperation . HashMap.Lazy.toList $ tmdb
-    _ <- Bloodhound.bulk operations
+search :: Bloodhound.Search -> IO ()
+search query = Bloodhound.withBH HTTP.Client.defaultManagerSettings server $ do
+  Monad.IO.liftIO $ putStrLn "Running query..."
+  response <- Bloodhound.searchByType tmdbIndex movieMapping query
+  let
+    bytes = HTTP.Client.responseBody response
+  searchResult <- case Aeson.eitherDecode bytes of
+    Left err -> fail $ "Unable to decode response: " ++ err
+    Right x  -> return (x :: Bloodhound.SearchResult Aeson.Object)
+  let hits = Bloodhound.hits . Bloodhound.searchHits $ searchResult
 
-    return ()
+  Monad.IO.liftIO $ putStrLn "Num\tRelevance Score\t\tMovie Title"
+  let
+    showMaybe :: Show a => Maybe a -> String
+    showMaybe Nothing  = ""
+    showMaybe (Just x) = show x
+
+    printHit :: (Int, Bloodhound.Hit Aeson.Object) -> Bloodhound.BH IO ()
+    printHit (num, hit) = Monad.IO.liftIO . putStrLn
+      $ show num
+      ++ "\t" ++ (showMaybe . Bloodhound.hitScore) hit
+      ++ "\t" ++ title
+      where
+      title :: String
+      title = case Bloodhound.hitSource hit of
+        Nothing -> ""
+        Just x ->
+          case HashMap.Lazy.lookup "title" x of
+            Just (Aeson.String t) -> Text.unpack t
+            _                     -> ""
+
+  mapM_ printHit $ zip [1..] hits
+
+  return ()
+
+server :: Bloodhound.Server
+server = Bloodhound.Server "http://localhost:9200"
+
+tmdbIndex :: Bloodhound.IndexName
+tmdbIndex = Bloodhound.IndexName "tmdb"
+
+movieMapping :: Bloodhound.MappingName
+movieMapping = Bloodhound.MappingName "movie"
 
 tmdbPath :: FilePath
 tmdbPath = "relevant-search-book/ipython/tmdb.json"
